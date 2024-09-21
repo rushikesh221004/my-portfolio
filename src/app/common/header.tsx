c0475254cedce879e67ef6f70d3735388a60c04e
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-09-21 19:44:52 +0530

    navbar work completed

--- src/app/common/header.tsx
+++ src/app/common/header.tsx
@@ -1,92 +1,45 @@
-"use client"
-import React from "react";
-import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-// import {AcmeLogo} from "./AcmeLogo.jsx";
-import Image from "next/image.js";
-import logo from "../assets/images/logo.png"
+"use client";
+
+import Image from "next/image";
+import logo from "../assets/images/logo.png";
+import { CiMenuFries } from "react-icons/ci";
+import { Button } from "@nextui-org/react";
+import { useState } from "react";
+import { MdRestaurantMenu } from "react-icons/md";
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
+  const [isClick, setIsClick] = useState(false)
   return (
-    <Navbar
-      isBordered
-      isMenuOpen={isMenuOpen}
-      onMenuOpenChange={setIsMenuOpen}
-    >
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-          <Image src={logo} alt="logo"/>
-        
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-        <Image src={logo} alt="logo"/>
-        
-        </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
+    <>
+      <div className="w-full px-3 py-4 lg:flex max-w-[1350px] 2xl:py-9 lg:py-6 lg:px-5 mx-auto lg:bg-transparent lg:items-center bg-[#252439]">
+        <div className="w-full flex justify-between items-center">
+          <Image src={logo} alt="logo" className="w-24 lg:w-36 2xl:w-44" />
+          {isClick === false && <CiMenuFries className="text-2xl font-extrabold lg:hidden" onClick={() => setIsClick(true)} />}
+          {isClick && <MdRestaurantMenu className="text-2xl font-extrabold lg:hidden" onClick={() => setIsClick(false)} />}
+        </div>
+        {isClick && <div className="w-full lg:hidden flex py-5 px-4 flex-col gap-2">
+          <p className="text-lg">Home</p>
+          <p className="text-lg">About</p>
+          <p className="text-lg">Portfolio</p>
+          <p className="text-lg">Service</p>
+          <p className="text-lg">Contact</p>
+          <p className="text-lg">Blog</p>
+          <Button radius="full" className="bg-gradient-to-tr w-fit mt-4 text-lg from-pink-500 to-yellow-500 text-white shadow-lg">
+            Download CV
           </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+        </div>}
+        <div className="hidden lg:flex lg:items-center lg:gap-8 2xl:gap-10 lg:flex-initial">
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold">Home</p>
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold ">About</p>
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold ">Portfolio</p>
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold ">Service</p>
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold ">Contact</p>
+          <p className="text-md font-bold 2xl:text-lg 2xl:font-semibold ">Blog</p>
+          <Button radius="full" className="bg-gradient-to-tr w-fit font-bold 2xl:text-lg 2xl:font-semibold 2xl:py-1 2xl:px-6 text-md from-pink-500 to-yellow-500 text-white shadow-lg">
+            Download CV
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
