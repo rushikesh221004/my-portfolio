236e289760d48febacc48e2ba9ea55539cd90099
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-09-21 20:24:37 +0530

    changes uncompleted

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@nextui-org/react";
+
+export default function Hero() {
+    return (
+        <div>
+            <div>
+                <div className="h-44 w-44 bg-white rounded-xl">
+
+                </div>
+            </div>
+            <div>
+                <p>Hello I'm</p>
+                <h1>Rushikesh Manjre</h1>
+                <p>Web Developer from Khamgaon</p>
+                <p>Jodi kokhono vul hoye jai tumi oporadh nio na ptate velit esse cillum dolore</p>
+                <div>
+                    <Button color="primary">
+                        Get a Quote
+                    </Button>
+
+                    <p>About Me</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
