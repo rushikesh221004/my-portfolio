8ccf09da3ce16b34d362db82640bb1604a39778a
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-09-22 08:34:43 +0530

    In hero section moile view work completed

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,24 +1,26 @@
 import { Button } from "@nextui-org/react";
+import myImg from "../assets/images/myImg.jpg"
+import Image from "next/image";
 
 export default function Hero() {
     return (
-        <div>
+        <div className="w-full px-6 justify-center h-full flex flex-col gap-8 items-center">
             <div>
-                <div className="h-44 w-44 bg-white rounded-xl">
-
+                <div className="h-44 w-44 bg-white rounded-xl overflow-hidden">
+                    <Image src={myImg} alt="myImg" className="w-fit h-fit object-cover"/>
                 </div>
             </div>
-            <div>
-                <p>Hello I'm</p>
-                <h1>Rushikesh Manjre</h1>
-                <p>Web Developer from Khamgaon</p>
-                <p>Jodi kokhono vul hoye jai tumi oporadh nio na ptate velit esse cillum dolore</p>
-                <div>
-                    <Button color="primary">
+            <div className="w-full flex flex-col gap-3">
+                <p className="font-bold text-lg text-center">Hello I'm</p>
+                <h1 className="text-center text-3xl font-bold">Rushikesh Manjre</h1>
+                <p  className="font-bold text-lg text-center">Web Developer from Khamgaon</p>
+                <p className="text-center">Jodi kokhono vul hoye jai tumi oporadh nio na ptate velit esse cillum dolore</p>
+                <div className="flex mt-6 items-center gap-4 w-full justify-center">
+                    <Button color="primary" className="text-lg font-semibold py-6 w-fit text-white px-6 rounded-full">
                         Get a Quote
                     </Button>
 
-                    <p>About Me</p>
+                    <p className="font-semibold text-lg">About Me</p>
 
                 </div>
             </div>
