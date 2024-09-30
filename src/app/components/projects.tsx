efaa08538016c38fcd4ba4661f299eb464e75c3e
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-10-01 00:06:53 +0530

    Create counter section work completed

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -19,20 +19,23 @@ export default function projects() {
 
     ]
     return (
-        <div className="w-full mb-20">
-            <div className="flex lg:flex-row flex-col px-4 lg:w-full lg:justify-center gap-12 lg:gap-32">
+        <div className="w-full mb-20 2xl:h-80 xl:flex xl:h-52">
+            <div className="flex lg:flex-row flex-col px-4 2xl:gap-56 lg:w-full xl:items-center xl:h-full lg:justify-center gap-12 lg:gap-32">
                 {projects.map((data, index) => (
                     <div className="flex gap-5" key={index}>
                         <div className="flex">
-                            <h2 className="text-red-500 tracking-[-1px] text-5xl font-medium">{data.projectCount}</h2>
-                            {index==2?null: <span className="text-red-500 font-extrabold text-3xl">+</span>}
+                            <h2 className="text-red-500 tracking-[-1px] text-5xl 2xl:text-7xl font-medium">{data.projectCount}</h2>
+                            {index == 2 ? null : <span className="text-red-500 font-extrabold text-3xl">+</span>}
                         </div>
-                        <div>
-                            <h2 className="text-xl font-bold">{data.porjectType}</h2>
-                            <h2 className="text-xl font-bold">{data.whichTypeProject}</h2>
+                        <div className="flex flex-col justify-center">
+                            <h2 className="text-xl font-bold 2xl:text-2xl">{data.porjectType}</h2>
+                            <h2 className="text-xl 2xl:text-2xl font-bold">{data.whichTypeProject}</h2>
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="h-52 w-52 2xl:w-80 rounded-l-xl 2xl:h-80 bg-[#6E7FD1] hidden xl:flex">
+
             </div>
         </div>
     )
